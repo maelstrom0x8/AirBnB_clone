bf7e436bcac9b87ff6725490731c71c9dcdfe06c
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-10-10 07:10:04 +0100

    Minor changes

--- console.py
+++ console.py
@@ -5,24 +5,65 @@ import cmd
 
 
 class CommandLineParser(cmd.Cmd):
+    """A command-line parser for interactive use.
+
+    This class provides a command-line interface for interactive use. Users can
+    enter commands, and this parser interprets and processes those commands.
+
+    Attributes:
+        prompt (str): The command prompt to display.
+    """
     prompt = '(hbnb) '
 
     def __init__(self, completekey="tab", stdin=None, stdout=None):
+        """Initialize the CommandLineParser.
+
+        Args:
+            completekey (str, optional): The key to trigger tab completion. Defaults to "tab".
+            stdin (file, optional): The input stream to use. Defaults to None.
+            stdout (file, optional): The output stream to use. Defaults to None.
+        """
         super().__init__(completekey, stdin, stdout)
 
     def do_quit(self, args):
-        "Exit the program"
+        """Quit the program.
+
+        Args:
+            args (str): Additional arguments (ignored).
+
+        Returns:
+            None
+        """
         sys.exit(0)
     
     def do_EOF(self, line):
-        """Handle EOF"""
+        """Handle EOF (End of File).
+
+        Args:
+            line (str): The current line of input.
+
+        Returns:
+            bool: True to indicate the end of input.
+        """
         return True
 
 
-def main(argc, argv, ioflag=True):
-    """Entrypoint"""
+def main(argc, argv, isstdin=True):
+    """Entrypoint for the command-line application
+
+    This function serves as the entry point for the command-line application. It creates
+    an instance of CommandLineParser and processes the provided arguments.
+
+    Args:
+        argc (int): The number of command-line arguments.
+        argv (list): A list of command-line arguments.
+        isstdin (bool, optional): Indicates whether input is from stdin. Defaults to True.
+
+    Returns:
+        None
+    """
     parser = CommandLineParser()
-    if not ioflag:
+    if not isstdin:
         parser.onecmd(argv)
     else:
         parser.cmdloop()
