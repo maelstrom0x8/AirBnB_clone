b240b5406be692e9302ff2848dd9eaf6bf01f391
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-10-10 06:39:31 +0100

    Add support for interactive and non-interactive modes

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import cmd
+
+
+class CommandLineParser(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def __init__(self, completekey="tab", stdin=None, stdout=None):
+        super().__init__(completekey, stdin, stdout)
+
+    def do_quit(self, args):
+        "Exit the program"
+        sys.exit(0)
+
+
+def main(argc, argv, ioflag=True):
+    """Entrypoint"""
+    parser = CommandLineParser()
+    if not ioflag:
+        parser.onecmd(argv)
+    else:
+        parser.cmdloop()
+
+
+if __name__ == '__main__':
+    import os
+
+    argc, argv = len(sys.argv), sys.argv
+
+    if not os.isatty(0):
+        argv = sys.stdin.read().strip('\n')
+        main(argc, argv, False)
+        sys.exit(1)
+
+    main(argc, argv)
