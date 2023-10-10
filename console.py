dc4f43061b7833f5ff5363ea1afc636c01f1f548
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-10-10 07:39:03 +0100

    Move parser logic into the cmd loop

--- console.py
+++ console.py
@@ -7,8 +7,9 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """A command-line parser for interactive use.
 
-    This class provides a command-line interface for interactive use. Users can
-    enter commands, and this parser interprets and processes those commands.
+    This class provides a command-line interface for interactive use.
+    Users can enter commands, and this parser interprets and processes
+    those commands.
 
     Attributes:
         prompt (str): The command prompt to display.
@@ -19,23 +20,18 @@ class HBNBCommand(cmd.Cmd):
         """Initialize the HBNBCommand.
 
         Args:
-            completekey (str, optional): The key to trigger tab completion. Defaults to "tab".
+            completekey (str, optional): The key to trigger tab completion.
+            Defaults to "tab".
             stdin (file, optional): The input stream to use. Defaults to None.
-            stdout (file, optional): The output stream to use. Defaults to None.
+            stdout (file, optional): The output stream to use.
+            Defaults to None.
         """
         super().__init__(completekey, stdin, stdout)
 
     def do_quit(self, args):
-        """Quit the program.
-
-        Args:
-            args (str): Additional arguments (ignored).
-
-        Returns:
-            None
-        """
+        """Quit command to exit the program"""
         sys.exit(0)
-    
+
     def do_EOF(self, line):
         """Handle EOF (End of File).
 
@@ -47,36 +43,40 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
 
+    def cmdloop(self, intro=None):
+        super().cmdloop(intro)
 
-def main(argc, argv, isstdin=True):
-    """Entrypoint for the command-line application
+    def process_args(self, argc, argv, isstdin=True):
+        """Entrypoint for the command-line application
 
-    This function serves as the entry point for the command-line application. It creates
-    an instance of HBNBCommand and processes the provided arguments.
+        This function serves as the entry point for the command-line
+        application. It creates an instance of HBNBCommand and processes
+        the provided arguments.
 
-    Args:
-        argc (int): The number of command-line arguments.
-        argv (list): A list of command-line arguments.
-        isstdin (bool, optional): Indicates whether input is from stdin. Defaults to True.
+        Args:
+            argc (int): The number of command-line arguments.
+            argv (list): A list of command-line arguments.
+            isstdin (bool, optional): Indicates whether input is from stdin.
+            Defaults to True.
 
-    Returns:
-        None
-    """
-    parser = HBNBCommand()
-    if not isstdin:
-        parser.onecmd(argv)
-    else:
-        parser.cmdloop()
+        Returns:
+            None
+        """
+        import os
+
+        argc, argv = len(sys.argv), sys.argv
+
+        if not os.isatty(0):
+            argv = sys.stdin.read().strip('\n')
+            self.process_args(argc, argv, False)
+            sys.exit(1)
+
+        self.process_args(argc, argv)
+        if not isstdin:
+            self.onecmd(argv)
+        else:
+            self.cmdloop()
 
 
 if __name__ == '__main__':
-    import os
-
-    argc, argv = len(sys.argv), sys.argv
-
-    if not os.isatty(0):
-        argv = sys.stdin.read().strip('\n')
-        main(argc, argv, False)
-        sys.exit(1)
-
-    main(argc, argv)
+    HBNBCommand().cmdloop()
