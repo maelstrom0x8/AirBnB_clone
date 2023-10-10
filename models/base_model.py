df00380a043ee883ca6ce81e56b14463ed800905
Chidimma <chidimmamofodum@gmail.com>
2023-10-09 22:14:32 -0400

    updating the base model init function to take kwargs and args

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 """A module that defines the BaseModel Class"""
 import uuid
-from datetime import datetime
+from datetime import datetime, date
 
 
 class BaseModel():
     """The baseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization function"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        valid_keys = ["created_at", "updated_at"]
+        if (kwargs is None or len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.id = kwargs.get("id", str(uuid.uuid4()))
+            for i in valid_keys:
+                setattr(self, i, datetime.fromisoformat(kwargs[i]))
 
     def __str__(self):
         """custom str function"""
