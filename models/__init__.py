90448bad49f4f3ef7ae8439ed1150792703b55da
Chidimma <chidimmamofodum@gmail.com>
2023-10-15 01:41:54 -0400

    making all files executable

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""The initialization module"""
+
 import models.engine.file_storage as s
 storage = s.FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,6 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Initialization function"""
-        valid_keys = ["created_at", "updated_at"]
         if (kwargs is None or len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
